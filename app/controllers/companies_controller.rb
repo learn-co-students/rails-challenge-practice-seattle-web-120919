c1b4f956d0881873f868ecae62af33d6956d6638
github username <github email address>
2020-01-16 23:41:58 -0800

    Done.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,39 @@
 class CompaniesController < ApplicationController
+    before_action :find_company, only: [:show, :edit]
+    
+    def index
+        @companies = Company.all
+    end
+
+    def new
+        @company = Company.new
+    end
+
+    def create
+        @company = Company.new(company_params)
+        if @company.save
+            redirect_to @company
+        else 
+            render :new 
+        end
+    end
+
+    def update
+        @company = Company.find(params[:id])
+        @company.update(company_params)
+        if @company.save
+            redirect_to @company
+        else
+            render :edit
+        end
+    end
+
+    private
+    def find_company
+        @company = Company.find(params[:id])
+    end
+
+    def company_params
+        params.require(:company).permit(:name, :country, :address, :rent_per_floor, :number_of_floors)
+    end
 end
