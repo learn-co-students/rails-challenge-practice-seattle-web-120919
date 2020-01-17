95ab4b0ac3acae33952443960c4819a4def3ee17
github username <github email address>
2020-01-17 00:21:39 -0800

    Done.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -34,6 +34,6 @@ class CompaniesController < ApplicationController
     end
 
     def company_params
-        params.require(:company).permit(:name, :country, :address, :rent_per_floor, :number_of_floors)
+        params.require(:company).permit(:name)
     end
 end
